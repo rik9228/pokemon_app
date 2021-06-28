b80759d0ddf19ade0cc81c271c7ff5096e93ed00
RIKU <rank228130@gmail.com>
2021-06-29 01:00:59 +0900

    feature: creaet pokemon_app

---
+++ src/components/Pokemon.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+    <h1>pokemon</h1>
+    <pokemon-search @setPokemonUrl="setPokemonUrl" :apiUrl="apiUrl"></pokemon-search>
+    <pokemon-list :imageUrl="imageUrl" :apiUrl="apiUrl" @setPokemonUrl="setPokemonUrl"></pokemon-list>
+    <pokemon-detail v-if="showDetail" :pokemonUrl="pokemonUrl" :imageUrl="imageUrl" @closeDetail="closeDetail"></pokemon-detail>
+  </div>
+</template>
+
+<script>
+import PokemonSearch from "@/components/PokemonSearch.vue";
+import PokemonList from "@/components/PokemonList.vue";
+import PokemonDetail from "@/components/PokemonDetail.vue";
+export default {
+  components: {
+    PokemonSearch,
+    PokemonList,
+    PokemonDetail,
+  },
+  data() {
+    return {
+      imageUrl: "https://pokeres.bastionbot.org/images/pokemon/",
+      apiUrl: "https://pokeapi.co/api/v2/pokemon/",
+      poekmonUrl: "",
+      showDetail: false,
+    };
+  },
+  methods: {
+    setPokemonUrl(url) {
+      this.pokemonUrl = url;
+      this.showDetail = true;
+    },
+    closeDetail() {
+      this.pokemonUrl = "";
+      this.showDetail = false;
+    },
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css?family=Acme");
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 10px;
+  width: calc(100% - 20px);
+  min-height: calc(100vh - 20px);
+  background: radial-gradient(#156f99, #0a2e50);
+  font-family: "Acme", arial;
+  font-size: 1rem;
+  font-weight: normal;
+}
+h1 {
+  color: #efefef;
+}
+</style>
---
+++ src/components/PokemonSearch.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="wrapper">
+    <form @submit.prevent="setPokemonUrl" action="">
+      <input type="text" v-model="searchValue" />
+    </form>
+    <i class="fas fa-search" @click="setPokemonUrl"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["apiUrl"],
+  data() {
+    return {
+      searchValue: "",
+    };
+  },
+  methods: {
+    setPokemonUrl() {
+      if (this.searchValue !== "") {
+        console.log(this.apiUrl + this.searchValue);
+        this.$emit("setPokemonUrl", this.apiUrl + this.searchValue);
+      }
+    },
+  },
+};
+</script>
+
+<style>
+.wrapper {
+  margin-bottom: 16px;
+}
+.searchbar {
+  position: relative;
+  width: 100%;
+  padding-bottom: 20px;
+}
+input {
+  border: none;
+  outline: none;
+  border-radius: 5px;
+  padding: 10px 40px 10px 10px;
+  width: calc(100% - 50px);
+  font-size: 1rem;
+  box-shadow: 0 15px 30px rgba(0, 0, 0, 0.2), 0 10px 10px rgba(0, 0, 0, 0.2);
+}
+i {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  font-size: 1.25rem;
+  color: #0a2e50;
+  cursor: pointer;
+}
+</style>
